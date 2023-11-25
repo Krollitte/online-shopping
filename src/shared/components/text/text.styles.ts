f805176cae3f56678de95cd3a18cdda7886bbc3d
RafaelSantos <rsantoslfap@gmail.com>
2023-11-25 16:07:08 -0300

    feat: insert text types

--- src/shared/components/text/text.styles.ts
+++ src/shared/components/text/text.styles.ts
@@ -3,10 +3,11 @@ import styled from "styled-components/native";
 interface ContainerTextProps {
   color?: string;
   size: string;
+  fontFamily: "Poppins-Bold" | "Poppins-Light" | "Poppins-Regular";
 }
 
 export const ContainerText = styled.Text<ContainerTextProps>`
   color: ${({ color }: ContainerTextProps) => (color ? `${color}` : "")};
   font-size: ${({ size }: ContainerTextProps) => size};
-  font-family: Poppins-Bold;
+  font-family: ${({ fontFamily }: ContainerTextProps) => fontFamily};
 `;
